c19a1e5b1e1a0fd4ade24766bdd002864805200e
ImGabreuw <imgabreuw.dev@gmail.com>
2025-07-20 20:31:08 -0300

    feat: implement length of last word calculation in SolutionV2

--- top-interview-150-java/src/p58_Length_of_Last_Word/SolutionV2.java
+++ top-interview-150-java/src/p58_Length_of_Last_Word/SolutionV2.java
@@ -1,28 +1,25 @@
 package p58_Length_of_Last_Word;
 
-public class Solution {
+public class SolutionV2 {
 
     public static void main(String[] args) {
         String input = "a";
-        Solution s = new Solution();
+        SolutionV2 s = new SolutionV2();
         int result = s.lengthOfLastWord(input);
         System.out.println(result);
     }
 
     public int lengthOfLastWord(String s) {
+        s = s.trim();
         int len = 0;
         for (int i = s.length() - 1; i >= 0; i--) {
             char c = s.charAt(i);
 
-            if (c == ' ') {
-                if (len != 0) {
-                    break;
-                }
-
-                continue;
+            if (c != ' ') {
+                ++len;
+            } else {
+                break;
             }
-
-            ++len;
         }
         return len;
     }
