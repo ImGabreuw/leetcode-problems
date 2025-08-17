fa9a80c0718a6129f2c788e4e04a3f283642bc50
ImGabreuw <imgabreuw.dev@gmail.com>
2025-08-16 22:42:37 -0300

    feat: optimize findMergePoint method for linked lists by reducing time complexity

--- programming-interview-questions-mycodeschool/src/find_merge_point_of_two_linked_list/Solution.java
+++ programming-interview-questions-mycodeschool/src/find_merge_point_of_two_linked_list/Solution.java
@@ -31,26 +31,54 @@ public class Solution {
     }
 
     /**
-     *
      * 1 -> 2 -> 3 -> 4 (list 1)
-     *      1 -> 2  / (list 2)
+     * 1 -> 2  / (list 2)
      */
 
-    Node findMergePoint(Node head1, Node head2) {
-        int m = length(head1);
-        int n = length(head2);
+//    Node findMergePoint(Node head1, Node head2) {
+//        int m = length(head1);
+//        int n = length(head2);
+//
+//        Node reference = head1;
+//        Node moving = head2;
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < n; j++) {
+//                if (reference == moving) {
+//                    return reference;
+//                }
+//                moving = moving.next;
+//            }
+//            reference = reference.next;
+//            moving = head2;
+//        }
+//
+//        return null;
+//    }
 
-        Node reference = head1;
-        Node moving = head2;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (reference == moving) {
-                    return reference;
-                }
-                moving = moving.next;
+    Node findMergePoint(Node head1, Node head2) {
+        int m = length(head1); // O(m)
+        int n = length(head2); // O(n)
+
+        int d = n - m;
+        if (m > n) {
+            Node temp = head1;
+            head1 = head2;
+            head2 = temp;
+            d = m - n;
+        }
+
+        // assuming head2 is bigger than head1
+
+        for (int i = 0; i < d; i++) {
+            head2 = head2.next;
+        }
+
+        while (head1 != null && head2 != null) {
+            if (head1 == head2) {
+                return head1;
             }
-            reference = reference.next;
-            moving = head2;
+            head1 = head1.next;
+            head2 = head2.next;
         }
 
         return null;
